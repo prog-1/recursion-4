90f6a65ab46af014e9981eec8d688afebe31f34d
Aleksandr Stukalov <stukalovsaleksandrs@gmail.com>
2022-10-12 19:30:36 +0300

    Added tests

--- exercises/main.go
+++ exercises/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"strings"
 )
 
@@ -63,7 +64,32 @@ func CorrectParentheses(s string) bool {
 	return result
 }
 
-// func main() {
-// 	s := "("
-// 	fmt.Println(CorrectParentheses(s))
-// }
+// GenParentheses generates all valid parentheses pair of length n*2.
+func GenParentheses(n int, f func(string)) {
+	var gen func(o, c int, s string)
+	gen = func(o, c int, s string) {
+		if o+c == 2*n {
+			if CorrectParentheses(s) {
+				f(s)
+			}
+			return
+		}
+		if o < n {
+			gen(o+1, c, s+"(")
+			gen(o+1, c, s+"[")
+			gen(o+1, c, s+"{")
+		}
+		if o > c {
+			gen(o, c+1, s+")")
+			gen(o, c+1, s+"]")
+			gen(o, c+1, s+"}")
+		}
+	}
+	gen(0, 0, "")
+}
+
+func main() {
+	var result []string
+	GenParentheses(3, func(s string) { result = append(result, s) })
+	fmt.Println(result)
+}
--- exercises/main_test.go
+++ exercises/main_test.go
@@ -1,13 +1,35 @@
 package main
 
+import (
+	"reflect"
+	"testing"
+)
+
 func TestCheck(t *testing.T) {
-	for t := range []struct{
-		input 
-		want bool
-	} {
+	for _, tc := range []struct {
+		input string
+		want  bool
+	}{
 		{"([{}])", true},
 		{"({[}])", false},
 	} {
-		
+		if got := CorrectParentheses(tc.input); got != tc.want {
+			t.Error("Got:", got, "Want:", tc.want)
+		}
+	}
+}
+
+func TestGen(t *testing.T) {
+	for _, tc := range []struct {
+		input uint32
+		want  []string
+	}{
+		{2, []string{"(())", "([])", "({})", "()()", "()[]", "(){}", "[()]", "[[]]", "[{}]", "[]()", "[][]", "[]{}", "{()}", "{[]}", "{{}}", "{}()", "{}[]", "{}{}"}},
+	} {
+		var got []string
+		GenParentheses(int(tc.input), func(s string) { got = append(got, s) })
+		if !reflect.DeepEqual(got, tc.want) {
+			t.Error("\nGot:", got, "\nWant:", tc.want)
+		}
 	}
 }
